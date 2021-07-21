900dbb6d6768198bf93a151a1e151cf0facbeb92
Rohan Kumar <rohanktalreja110@gmail.com>
2021-07-22 01:11:04 +0500

    make github api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 import "../styles/App.css";
+import axios from "axios";
 import React, { Component } from "react";
 import Navbar from "./layouts/Navbar";
 import UserList from "./users/UserList";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { loading, users } = this.state;
     return (
       <div>
         <Navbar />
         <div className="container">
-          <UserList />
+          <UserList loading={loading} users={users} />
         </div>
       </div>
     );
