15c232856110ca831b405fe92cc4e17c3130bdd4
Rohan Kumar <rohanktalreja110@gmail.com>
2021-07-22 17:14:10 +0500

    add links to home and about page

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
   const { icon, title } = props;
@@ -8,6 +9,14 @@ function Navbar(props) {
       <h1>
         {title} <i className={icon} />
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
